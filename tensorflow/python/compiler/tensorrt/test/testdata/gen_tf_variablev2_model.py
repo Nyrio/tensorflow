69e7e6015a179e9b8a29eb1d208d436d7f69f461
Louis Sugy <lsugy@nvidia.com>
2022-03-30 07:18:47 -0700

    Replace tensorflow.compat.v1 imports

--- tensorflow/python/compiler/tensorrt/test/testdata/gen_tf_variablev2_model.py
+++ tensorflow/python/compiler/tensorrt/test/testdata/gen_tf_variablev2_model.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
-import tensorflow.compat.v1 as tf1
+from tensorflow.python.client import session
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import variables
 from tensorflow.python.saved_model import builder
 from tensorflow.python.saved_model import signature_constants
 from tensorflow.python.saved_model import signature_def_utils
 from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model import utils
 
-tf1.disable_eager_execution()
-tf1.disable_resource_variables()
+ops.disable_eager_execution()
+variable_scope.disable_resource_variables()
 
 def GenerateModelWithVariableV2(tf_saved_model_dir):
   """Generate a model with a VariableV2 node using TFv1 API."""
@@ -23,10 +25,10 @@ def GenerateModelWithVariableV2(tf_saved_model_dir):
           dtype=dtypes.float32, shape=[None, 1, 1], name="input1")
       input2 = array_ops.placeholder(
           dtype=dtypes.float32, shape=[None, 1, 1], name="input2")
-      var1 = tf1.get_variable('var1', shape=[1, 1, 1],
+      var1 = variable_scope.get_variable('var1', shape=[1, 1, 1],
                               initializer=tf.constant_initializer([[[13.]]]),
                               dtype=tf.float32)
-      var2 = tf1.get_variable('var2', shape=[1, 1, 1],
+      var2 = variable_scope.get_variable('var2', shape=[1, 1, 1],
                               initializer=tf.constant_initializer([[[37.]]]),
                               dtype=tf.float32)
       mul1 = input1 * var1
@@ -49,8 +51,8 @@ def GenerateModelWithVariableV2(tf_saved_model_dir):
       outputs={"output": utils.build_tensor_info(out)},
       method_name=signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY)
   saved_model_builder = builder.SavedModelBuilder(tf_saved_model_dir)
-  with tf1.Session(graph=g) as sess:
-    tf1.global_variables_initializer().run()
+  with session.Session(graph=g) as sess:
+    variables.global_variables_initializer().run()
     saved_model_builder.add_meta_graph_and_variables(
         sess, [tag_constants.SERVING],
         signature_def_map={
